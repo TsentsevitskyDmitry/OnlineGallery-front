b0ca85e3861f40af65508653404efdfa5a9fcb10
The DAS Creator <do.not@email.me>
2020-05-10 16:12:08 +0300

    Clean working tree

--- views/GaleryView.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useCallback } from "react";
-import Gallery from "../gallery/Gallery";
-
-const GaleryView = ({ isOwner, Container, views, onClick, onAction }) => {
-		return <Gallery isOwner={isOwner} Container={Container} photos={views} onClick={onClick} onAction={onAction}/>;
-};
-
-export default GaleryView;
