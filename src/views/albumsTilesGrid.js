2a032b7c13c24ad866ca32eb05bb7c334c34f222
TsentsevitskyDmitry <55098562+TsentsevitskyDmitry@users.noreply.github.com>
2020-05-10 15:06:47 +0200

    Some fixes

--- src/views/albumsTilesGrid.js
+++ src/views/albumsTilesGrid.js
@@ -1,19 +1,19 @@
 export const tilesGrid = [
   {
-    width: 1,
-    height: 1,
-  },
-  {
-    width: 6,
-    height: 4,
-
-  },
-  {
     width: 5,
     height: 3,
   },
   {
-    width: 5,
+    width: 4,
+    height: 3,
+
+  },
+  {
+    width: 1,
+    height: 1,
+  },
+  {
+    width: 4,
     height: 3,
   },
   {
